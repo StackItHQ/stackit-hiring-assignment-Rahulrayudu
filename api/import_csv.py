218e0d46171c1bda06c63a3db45ef71718ff82d6
Rahul Sandireddy <76141102+Rahulrayudu@users.noreply.github.com>
2023-10-01 15:50:16 +0530

    API

---
+++ api/import_csv.py
@@ -0,0 +1,21 @@
+
+import os
+from Google import Create_Service
+import gspread
+import csv
+
+FOLDER_PATH = 'C:/Users/sandi/Documents/'
+CLIENT_SECRET_FILE = os.path.join(FOLDER_PATH, 'client_secret_317110996643-e4i4h3kp3nmpb19pradpmto1m5dhe29r.apps.googleusercontent.com.json')
+API_SERVICE_NAME = 'sheets'
+API_VERSION = 'v4'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+service = Create_Service(CLIENT_SECRET_FILE, API_SERVICE_NAME, API_VERSION, SCOPES)
+
+spreadsheet_id = "13k7aQPXNujk9hBH1SIO9-Ff_B1auPVGu_cbLNJew2H0"  # Please put your Spreadsheet ID.
+sheet_name = "Sheet1"  # Please put the sheet ID of the sheet you want to use.
+csv_file = "C:/Users/sandi/Downloads/crash_catalonia.csv"  # Please put the file path of the CSV file you want to use.
+
+f = open(csv_file, "r")
+values = [r for r in csv.reader(f)]
+request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=sheet_name, valueInputOption="USER_ENTERED", body={"values": values}).execute()
